d8723633f7595b49570145d9ed2c782af5a8d03f
fmejiar <frankmejiarios@gmail.com>
2021-04-07 20:18:32 -0500

    Adding false schema export in the room database.

--- app/src/main/java/com/fmejiar/drinksapp/AppDatabase.kt
+++ app/src/main/java/com/fmejiar/drinksapp/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import com.fmejiar.drinksapp.data.model.DrinkEntity
 import com.fmejiar.drinksapp.domain.DrinkDao
 
-@Database(entities = [DrinkEntity::class], version = 1)
+@Database(entities = [DrinkEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun drinkDao(): DrinkDao
